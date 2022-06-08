9bb4519d073fc10f71d55b156173ac0a7b88678c
Uros Novakovic <uros.novakovic1996@hotmail.com>
2022-06-08 18:23:51 +0200

    Update index.js
    Uvedene komande za kretanje trenutnih kockica (igraca) levo, desno, skok

--- index.js
+++ index.js
@@ -10,45 +10,48 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 //Izrada boraca
 
-const gravity = 0.1
+const gravity = 0.5
 
 class Sprite {
-    constructor({position, velocity}){
-        this.position = position
-        this.velocity = velocity
+    constructor({pozicija, brzina}){
+        this.pozicija = pozicija
+        this.brzina = brzina
         this.height = 150
+        this.lastKey
     }
     draw() {
         c.fillStyle = `red`
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.pozicija.x, this.pozicija.y, 50, 150)
     }
     update(){
         this.draw()
-        this.position.y += this.velocity.y
 
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else this.velocity.y += gravity
+        this.pozicija.x += this.brzina.x
+        this.pozicija.y += this.brzina.y
+
+        if (this.pozicija.y + this.height + this.brzina.y >= canvas.height) {
+            this.brzina.y = 0
+        } else this.brzina.y += gravity
     }
 }
 
 const igrac = new Sprite({
-    position: {
+    pozicija: {
         x:0,
     y:0
     },
-    velocity: {
+    brzina: {
         x:0,
         y:0
     }
     })
 
 const protivnik = new Sprite({
-position: {
+    pozicija: {
     x:400,
 y:100
 },
-velocity: {
+brzina: {
     x:0,
     y:0
 }
@@ -56,13 +59,96 @@ velocity: {
 
 console.log(igrac)
 
+const keys = {
+    a: {
+      pressed: false
+    },
+    d: {
+      pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+      },
+      ArrowLeft: {
+        pressed: false
+      }
+  }
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = `black`
     c.fillRect(0, 0, canvas.width, canvas.height)
     igrac.update()
     protivnik.update()
-    
-}
+
+    igrac.brzina.x = 0
+    protivnik.brzina.x = 0
+
+ // Kretanje igraca
+    if (keys.a.pressed && igrac.lastKey === 'a') {
+        igrac.brzina.x = -5
+     } else if (keys.d.pressed && igrac.lastKey === 'd') {
+                igrac.brzina.x = 5
+            }
+//Kretanje protivnika
+    if (keys.ArrowLeft.pressed && protivnik.lastKey === 'ArrowLeft') {
+         protivnik.brzina.x = -5
+     } else if (keys.ArrowRight.pressed && protivnik.lastKey === 'ArrowRight') {
+         protivnik.brzina.x = 5
+      }
+        }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    switch (event.key) {
+        case 'd':
+        keys.d.pressed = true
+        igrac.lastKey = 'd'
+        break
+        case 'a':
+         keys.a.pressed = true
+         igrac.lastKey = 'a'
+         break
+         case 'w':
+            igrac.brzina.y = -20
+            break
+
+            case 'ArrowRight':
+                keys.ArrowRight.pressed = true
+                protivnik.lastKey = 'ArrowRight'
+                break
+                case 'ArrowLeft':
+                keys.ArrowLeft.pressed = true
+                protivnik.lastKey = 'ArrowLeft'
+                break
+                 case 'ArrowUp':
+                    protivnik.brzina.y = -20
+                    break
+    }
+    console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+        break
+        case 'a':
+        keys.a.pressed = false
+        break
+       
+    }
+    // Protivnik komande
+    switch(event.key) {
+        case 'ArrowRight':
+        keys.ArrowRight.pressed = false
+        break
+        case 'ArrowLeft':
+        keys.ArrowLeft.pressed = false
+        break
+    }
+
+    console.log(event.key)
+})
